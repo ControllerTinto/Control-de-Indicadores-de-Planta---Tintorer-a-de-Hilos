e8e2b469e0bf4a43ea8c09208c6b8cfd68b28e2f
ControllerTinto <william.pareja@nettalco.com.pe>
2026-06-23 10:27:35 -0500

    Create session.py

---
+++ CORE/session.py
@@ -0,0 +1,10 @@
+# 🔁 Estado del lote en memoria (control de operación)
+
+lote = {
+    "ob": None,              # 🧾 lote actual
+    "proceso": None,        # 🔄 etapa
+    "material": None,       # 🧵 material
+    "tipo_soporte": None,   # 🧱 soporte físico
+    "maquina": None,        # ⚙️ máquina seleccionada
+    "registros": []        # 📊 registros por huso
+}

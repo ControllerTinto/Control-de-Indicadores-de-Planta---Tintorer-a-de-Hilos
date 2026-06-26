ab0c74721f9c7451bb23e89583f4c955a1b15bf9
ControllerTinto <william.pareja@nettalco.com.pe>
2026-06-26 11:45:39 -0500

    Create HusosInoperativos.js

---
+++ src/pages/HusosInoperativos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HusosInoperativos() {
+  return <div style={{ color: '#fff' }}>Husos Inoperativos — en construcción</div>;
+}
+
+export default HusosInoperativos;

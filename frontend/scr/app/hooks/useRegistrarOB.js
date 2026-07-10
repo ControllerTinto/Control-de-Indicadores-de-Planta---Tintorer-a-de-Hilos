a6127968e4e2eaa433e8ec2132270c98f631116a
ControllerTinto <william.pareja@nettalco.com.pe>
2026-07-10 13:39:12 -0500

    Delete frontend/scr/app/hooks directory

--- frontend/scr/app/hooks/useRegistrarOB.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000/api";
-
-export async function registrarOB(datos) {
-  const res = await axios.post(`${API_URL}/ob/registrar`, datos);
-  return res.data;
-}
